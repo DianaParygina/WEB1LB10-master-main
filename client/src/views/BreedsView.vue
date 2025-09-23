c80b118f4fa460f38661df41c9655db7cb64d22b
Diana <89041508784d@gmail.com>
2025-09-23 13:56:48 +0800

    test_commit

--- client/src/views/BreedsView.vue
+++ client/src/views/BreedsView.vue
@@ -124,6 +124,7 @@ onBeforeMount(() => {
           <div class="modal-header">
             <h1 class="modal-title fs-5" id="exampleModalLabel">
               Редактировать породу
+              Test
             </h1>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
